bdbd7814871101e3394b13fb2c51b1c1901feab6
madhu910 <83641911+madhu910@users.noreply.github.com>
2025-09-05 18:27:44 +0530

    Create db.js

---
+++ db.js
@@ -0,0 +1,10 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
